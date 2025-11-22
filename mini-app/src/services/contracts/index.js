ee9ae4414deba9f0356bf443bca1bbd9a0a98b22
winsznx <timjosh507@gmail.com>
2025-11-22 22:13:52 +0100

    Add contract services barrel export
      - Export all contract service instances
      - Provide centralized import point for components
      - Include groupVaultFactory, groupVault, member
      services

---
+++ mini-app/src/services/contracts/index.js
@@ -0,0 +1,8 @@
+/**
+ * Contract Services Index
+ * Exports all contract interaction services
+ */
+
+export { default as groupVaultFactory } from './groupVaultFactory';
+export { default as groupVault } from './groupVault';
+export { default as member } from './member';
